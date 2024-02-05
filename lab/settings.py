a213e97c6824f0cb6c0f7acbfdd915d598f5fd14
imroz_khan <kimu7466@gmail.com>
2024-02-05 12:23:50 +0530

    #

--- lab/settings.py
+++ lab/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-ux!e))5uzaz(=7z245k$ew+__2u)8g2d!p+r3fqvc^g6s&15m0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
