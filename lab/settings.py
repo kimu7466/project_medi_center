fd8178ca23c73e1e56a61e69b1086828bc0fdc0d
imroz_khan <kimu7466@gmail.com>
2024-02-05 12:39:25 +0530

    email_config

--- lab/settings.py
+++ lab/settings.py
@@ -142,5 +142,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'brijeshgondaliya.tops@gmail.com'
-EMAIL_HOST_PASSWORD = 'siahuhxsntfmclxd'
+EMAIL_HOST_USER = 'kimu7466@gmail.com'
+EMAIL_HOST_PASSWORD = 'eklnfkxuvopgvotc'
